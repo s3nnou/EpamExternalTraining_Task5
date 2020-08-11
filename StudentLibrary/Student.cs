326fe3c992bf93b8c41d75c9fe787479e6fdad55
Uladlen <s3nnou@gmail.com>
2020-08-11 12:21:14 +0300

    task 1 is mostly done

--- StudentLibrary/Student.cs
+++ StudentLibrary/Student.cs
@@ -2,7 +2,83 @@
 
 namespace StudentLibrary
 {
-    public class Student
+    /// <summary>
+    /// Class for Student representation
+    /// </summary>
+    public class Student: IComparable
     {
+        /// <summary>
+        /// Student name
+        /// </summary>
+       public string Name { get; set; }
+
+        /// <summary>
+        /// Test name
+        /// </summary>
+       public string Test { get; set; }
+
+        /// <summary>
+        /// Result
+        /// </summary>
+       public UInt16 Result { get; set; }
+
+        /// <summary>
+        /// Test Date
+        /// </summary>
+       DateTime Date { get; set; }
+
+        /// <summary>
+        /// Method for Student objects class comparasion
+        /// </summary>
+        /// <param name="obj">Object to compare</param>
+        /// <returns>true if equal, false is not</returns>
+        public override bool Equals(object obj)
+        {
+           if(obj is Student)
+            {
+                Student student = obj as Student;
+
+                if(this.Name == student.Name && this.Result == student.Result && this.Test == student.Test && this.Date == Date)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Method for hashcode calculation
+        /// </summary>
+        /// <returns>HashCode</returns>
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode() ^ Test.GetHashCode() ^ Result.GetHashCode() ^ Date.GetHashCode();
+        }
+
+        /// <summary>
+        /// Method for IComparable implimetation
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns>1 - if bigger, 0 - if equal, -1 - if not</returns>
+        public int CompareTo(object obj)
+        {
+            return Result.CompareTo(((Student)obj).Result);
+        }
+
+        /// <summary>
+        /// Returns Student data in a text format
+        /// </summary>
+        /// <returns>text</returns>
+        public override string ToString()
+        {
+            return string.Format($"{Name} - {Test} - {Result} - {Date}");
+        }
     }
 }
--- Tree/Tree.cs
+++ Tree/Tree.cs
@@ -1,14 +1,33 @@
 ﻿using StudentLibrary;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using System.Xml.Serialization;
 
 namespace Tree
 {
+    [Serializable]
+    /// <summary>
+    /// This is a class for a binary tree
+    /// </summary>
+    /// <typeparam name="T">Type is everything that can be compared</typeparam>
     class Tree<T> where T : IComparable<T>
     {
-        Node<T> Root { get; set; }
+        /// <summary>
+        /// Node to store a Root of a Tree
+        /// </summary>
+        private Node<T> Root { get; set; }
 
+        /// <summary>
+        /// List for balancing
+        /// </summary>
+        private List<Node<T>> NodesList { get; set; }
+
+        /// <summary>
+        /// Method to Adding a new Node to a Tree
+        /// </summary>
+        /// <param name="value">Inserted value</param>
         public void Add(T value)
         {
             if(Root != null)
@@ -50,6 +69,81 @@ namespace Tree
             {
                 Root = new Node<T>(value);
             }
+        }
+
+
+        /// <summary>
+        /// Method for a Tree Balancing
+        /// </summary>
+        public void Balance()
+        {
+            void ConvertTreeToList(Node<T> parent)
+            {
+                if(parent != null)
+                {
+                    ConvertTreeToList(parent.Left);
+                    NodesList.Add(parent);
+                    ConvertTreeToList(parent.Right);
+                }
+            }
+
+            Node<T> NewParentCreation(List<Node<T>> nodes, int start, int finish)
+            {
+                if (start > finish)
+                {
+                    return null;
+                }
+
+                int middle = (start + finish) / 2;
+                Node<T> newParent= nodes[middle];
+
+                newParent.Left = NewParentCreation(nodes, start, middle - 1);
+                newParent.Right = NewParentCreation(nodes, middle + 1, finish);
+
+                return newParent;
+            }
+
+            ConvertTreeToList(Root);
+
+            int newStart = NodesList.Count - ((NodesList.Count - 1) / 2);
+
+            Node<T> newRoot = NodesList[newStart];
+
+            newRoot.Left = NewParentCreation(NodesList, 0, newStart - 1);
+            newRoot.Right = NewParentCreation(NodesList, newStart + 1, NodesList.Count - 1);
+
+            Root = newRoot;
+        } 
+
+        /// <summary>
+        /// Method for a serialization
+        /// </summary>
+        /// <param name="path">Output file path</param>
+        public void Serialize(string path)
+        {
+            using(TextWriter writer = new StreamWriter(path))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(Tree<T>));
+                serializer.Serialize(writer, this);
+            }
+        }
+
+        /// <summary>
+        /// Method for a deserialization
+        /// </summary>
+        /// <param name="path">Input File path</param>
+        /// <returns>new tree</returns>
+        public Tree<T> Deserialize(string path)
+        {
+            Tree<T> binaryTree;
+
+            using (Stream reader = new FileStream(path, FileMode.Open))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(Tree<T>));
+                binaryTree = (Tree<T>)serializer.Deserialize(reader);
+            }
+
+            return binaryTree;
         }
     }
 }
